66b8fd97f0a1536b5dad98c139dfd6dcabdc1a9a
caphilpott <70985203+caphilpott@users.noreply.github.com>
2021-04-25 17:36:29 -0500

    update stroke folder

--- Stroke/app.py
+++ Stroke/app.py
@@ -5,18 +5,19 @@ import numpy as np
 import os
 from xgboost import XGBClassifier
 from modelHelper import ModelHelper
+import logging
 
 #init app and class
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 modelHelper = ModelHelper()
 
-#endpoint  
-# Favicon      
-#@app.route('/favicon.ico')
-#def favicon():
-#    return send_from_directory(os.path.join(app.root_path, 'static'),
-#                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+#endpoint
+# Favicon    
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 # Route to render index.html template
 @app.route("/")
@@ -80,3 +81,5 @@ def add_header(r):
 #main
 if __name__ == "__main__":
     app.run(debug=True)
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.ERROR)
