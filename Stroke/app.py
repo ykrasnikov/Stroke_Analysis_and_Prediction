02a3119f2933652fe38f132d678d06f0fde49583
caphilpott <70985203+caphilpott@users.noreply.github.com>
2021-04-25 01:02:05 -0500

    update app.py

--- Stroke/app.py
+++ Stroke/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 modelHelper = ModelHelper()
 
-#endpoint
+#endpoint  
 # Favicon      
 #@app.route('/favicon.ico')
 #def favicon():
