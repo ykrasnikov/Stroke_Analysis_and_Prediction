ef3400051fe4ea7d61f6ede37b24885436dd3d44
caphilpott <70985203+caphilpott@users.noreply.github.com>
2021-05-01 14:42:46 -0500

    refresh for folder restructure

--- Stroke/modelHelper.py
+++ Stroke/modelHelper.py
@@ -3,59 +3,48 @@ import datetime
 import time
 import pickle
 import numpy as np
-from xgboost import XGBClassifier
 
 class ModelHelper():
     def __init__(self):
         pass
 
-    def makePredictions(self, age, hypertension, heart_disease, avg_glucose_level, bmi, sex_label,  married_label, residence_label, work_type, smoke_hist):
-        work_type_govt_job = 0
-        work_type_never_worked = 0
-        work_type_private = 0
-        work_type_self_employed = 0
-        work_type_children = 0
+    def makePredictions(self, sex_flag, age, fare, familySize, pclass, embarked):
+        pclass_1 = 0
+        pclass_2 = 0
+        pclass_3 = 0
 
-        smoking_status_unknown = 0
-        smoking_status_formerly_smoked = 0
-        smoking_status_never_smoked = 0
-        smoking_status_smokes = 0
+        embarked_c = 0
+        embarked_q = 0
+        embarked_s = 0
 
-        # parse work type
-        if (work_type == 1):
-            work_type_govt_job = 1
-        elif (work_type == 2):
-            work_type_never_worked = 1
-        elif (work_type == 3):
-            work_type_private = 1
-        elif (work_type == 4):
-            work_type_self_employed = 1
-        elif (work_type == 5):
-            work_type_children = 1
+        # parse pclass
+        if (pclass == 1):
+            pclass_1 = 1
+        elif (pclass == 2):
+            pclass_2 = 1
+        elif (pclass == 3):
+            pclass_3 = 1
         else:
             pass
 
-        # parse smoke history
-        if (smoke_hist == 1):
-            smoking_status_unknown = 1
-        elif (smoke_hist == 2):
-            smoking_status_formerly_smoked = 1
-        elif (smoke_hist == 3):
-            smoking_status_never_smoked = 1
-        elif (smoke_hist == 4):
-            smoking_status_smokes = 1
+        # parse embarked
+        if (embarked == "C"):
+            embarked_c = 1
+        elif (embarked == "Q"):
+            embarked_q = 1
+        elif (embarked == "S"):
+            embarked_s = 1
         else:
             pass
-       
 
-        input_pred = [[age, hypertension, heart_disease, avg_glucose_level, bmi, sex_label,  married_label, residence_label, work_type_govt_job, work_type_never_worked, work_type_private, work_type_self_employed, work_type_children, smoking_status_unknown, smoking_status_formerly_smoked, smoking_status_never_smoked, smoking_status_smokes]]
-        pass
+        input_pred = [[sex_flag, age, fare, familySize, pclass_1, pclass_2, pclass_3, embarked_c, embarked_q, embarked_s]]
+
 
         filename = 'finalized_model.sav'
         ada_load = pickle.load(open(filename, 'rb'))
 
         X = np.array(input_pred)
         preds = ada_load.predict_proba(X)
-        preds_singular = ada_load.predict_proba(X)[:,1]
+        preds_singular = ada_load.predict(X)
 
         return preds_singular[0]
