ac8668549429a18b4561f1e41e1c6d99ef8eb1ca
ykrasnikov <ykrasnikov@gmail.com>
2021-05-01 13:32:37 -0500

    web development

--- Stroke/static/js/app3.js
+++ Stroke/static/js/app3.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-    buildTable(); 
+$(document).ready(function () {
+    buildTable();
     //Event Listeners
-    $("#filter-btn").on("click", function(e) {
+    $("#filter-btn").on("click", function (e) {
         e.preventDefault();
         buildTable();
     });
-    $("#filter-clear").on("click", function(e) {
+    $("#filter-clear").on("click", function (e) {
         e.preventDefault();
         resetFilters();
         buildTable();
     });
-    $("#form").on("submit", function(e) {
+    $("#form").on("submit", function (e) {
         e.preventDefault();
         buildTable();
-    });   
-}); 
+    });
+});
 
 // id is reference here
 function resetFilters() {
@@ -26,23 +26,23 @@ function resetFilters() {
 
     $("#heart_disease_choice").val("");
 
-    $("#ever_married_choice").val("");    
+    $("#ever_married_choice").val("");
 
     $("#smoking_status_choice").val("");
 
     $("#work_type_choice").val("");
 
-    $("#residence_type_choice").val("");    
+    $("#residence_type_choice").val("");
 
 
 }
 
 function buildTable() {
     // live path
-    d3.csv("static/data/healthcare-dataset-stroke-data.csv").then(function(strokeData) {
+    d3.csv("static/data/healthcare-dataset-stroke-data.csv").then(function (strokeData) {
 
-    // practice path
-    // d3.csv("../static/data/healthcare-dataset-stroke-data.csv").then(function(strokeData) {
+        // practice path
+        // d3.csv("../static/data/healthcare-dataset-stroke-data.csv").then(function(strokeData) {
 
         var genderFilter = $("#gender_choice").val();
         var hypertensionFilter = ($("#hypertension_choice").val());
@@ -59,31 +59,31 @@ function buildTable() {
 
         if (genderFilter) {
             filteredData = filteredData.filter(row => (row.gender) === (genderFilter));
-            }
+        }
         if (hypertensionFilter) {
             filteredData = filteredData.filter(row => (row.hypertension) === (hypertensionFilter));
-            } 
+        }
         if (heartdiseaseFilter) {
             filteredData = filteredData.filter(row => (row.heart_disease) === (heartdiseaseFilter));
-            } 
+        }
         if (marriedFilter) {
             filteredData = filteredData.filter(row => (row.ever_married) === (marriedFilter));
-            } 
+        }
         if (workFilter) {
             filteredData = filteredData.filter(row => (row.work_type) === (workFilter));
-            } 
+        }
         if (residenceFilter) {
             filteredData = filteredData.filter(row => (row.Residence_type) === (residenceFilter));
-            } 
+        }
         if (smokingFilter) {
             filteredData = filteredData.filter(row => (row.smoking_status) === (smokingFilter));
-            } 
+        }
         if (strokeFilter) {
             filteredData = filteredData.filter(row => (row.stroke) === (strokeFilter));
-            } 
+        }
 
         buildTableString(filteredData);
-    }); 
+    });
 }
 
 function buildTableString(strokeData) {
@@ -96,15 +96,15 @@ function buildTableString(strokeData) {
     //destroy datatable
     $("table").DataTable().clear().destroy();
 
-    var datarows = strokeData.map(x => [x["id"], x["gender"], x["age"], x["hypertension"], x["heart_disease"], x["ever_married"] ,x["work_type"],x["Residence_type"],x ["avg_glucose_level"],x["bmi"],x["smoking_status"],x["stroke"]])
+    var datarows = strokeData.map(x => [x["id"], x["gender"], x["age"], x["hypertension"], x["heart_disease"], x["ever_married"], x["work_type"], x["Residence_type"], x["avg_glucose_level"], x["bmi"], x["smoking_status"], x["stroke"]])
 
     //redraw
     $("#table").DataTable({
 
         data: datarows,
-        "defaultContent": "", 
+        "defaultContent": "",
 
-        "pageLength": 20, 
+        "pageLength": 20,
         dom: 'Bfrtip', //lbfrtip if you want the length changing thing
         buttons: [
             { extend: 'copyHtml5' },
@@ -112,7 +112,7 @@ function buildTableString(strokeData) {
             { extend: 'csvHtml5' },
             {
                 extend: 'pdfHtml5',
-                title: function() { return "Stroke Data"; },
+                title: function () { return "Stroke Data"; },
                 orientation: 'portrait',
                 pageSize: 'LETTER',
                 text: 'PDF',
@@ -120,5 +120,5 @@ function buildTableString(strokeData) {
             }
         ]
     });
-}; 
+};
 
